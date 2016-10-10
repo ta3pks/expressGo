cf83e2763edb8639535a1460f7565890b4bf612d
nikos <nikos.efthias@gmail.com>
2016-10-10 05:34:53 +0400

    middleWares are ready to use

---
+++ TODO.md
@@ -0,0 +1,7 @@
+Bugs
+=
+* [ ] Prevent Double execution on early exited middlewares
+
+TODOS
+=
+* [ ] Make handler signature simpler by seperating the middlewares and handlers
--- express.go
+++ express.go
@@ -10,13 +10,36 @@ import (
 //__ROUTER
 
 type Params interface{}
-type Handle func(http.ResponseWriter, *http.Request, Params)
+type NextFunc func() Handle
+//TODO:  Change signature of middlewares so user does not have to write nextfync and return for actual Handle
+type Handle func(http.ResponseWriter, *http.Request, Params, NextFunc) Handle
 
 //Route describes the requirements of each subroute
 type Route struct {
 	Path   string
 	Method string
-	Func   Handle
+	Func   []Handle
+}
+
+func iterateMiddleWare(handlers *[]Handle, res http.ResponseWriter, req *http.Request, prm Params) Handle {
+	if len(*handlers) == 1 {
+		return (*handlers)[0]
+	}
+	maxIndex := len(*handlers) - 1
+	for index, val := range *handlers {
+		hasNext := false
+		if index != maxIndex {
+			hasNext = true
+		}
+		if !hasNext {
+			return val
+		}
+		// TODO: Prevent Double execution
+		if next := val(res, req, prm, func() Handle { return (*handlers)[index+1] }); next == nil {
+			return val
+		}
+	}
+	return nil
 }
 
 //Mounter is basic router mount struct
@@ -55,40 +78,41 @@ func (mounter *Mounter) Mount(router *rtr.Router) {
 		switch strings.ToLower(route.Method) {
 		case "get":
 			router.GET(path, func(res http.ResponseWriter, req *http.Request, params rtr.Params) {
-				fnc(res, req, params)
+				iterateMiddleWare(&fnc,res,req,params)(res,req,params,nil)
 			})
 		case "post":
 			router.POST(path, func(res http.ResponseWriter, req *http.Request, params rtr.Params) {
-				fnc(res, req, params)
+				iterateMiddleWare(&fnc,res,req,params)(res,req,params,nil)
 			})
 		case "delete":
 			router.DELETE(path, func(res http.ResponseWriter, req *http.Request, params rtr.Params) {
-				fnc(res, req, params)
+				iterateMiddleWare(&fnc,res,req,params)(res,req,params,nil)
 			})
 		case "put":
 			router.PUT(path, func(res http.ResponseWriter, req *http.Request, params rtr.Params) {
-				fnc(res, req, params)
+				iterateMiddleWare(&fnc,res,req,params)(res,req,params,nil)
 			})
 		}
 	}
 }
 
 //GET request handler
-func (mounter *Mounter) GET(path string, Func Handle) {
-	mounter.Routes = append(mounter.Routes, &Route{path, "get", Func})
+func (mounter *Mounter) GET(path string, Funcs ...Handle) {
+
+	mounter.Routes = append(mounter.Routes, &Route{path, "get", Funcs})
 }
 
 //POST request handler
-func (mounter *Mounter) POST(path string, Func Handle) {
-	mounter.Routes = append(mounter.Routes, &Route{path, "post", Func})
+func (mounter *Mounter) POST(path string, Funcs ...Handle) {
+	mounter.Routes = append(mounter.Routes, &Route{path, "post", Funcs})
 }
 
 //PUT request handler
-func (mounter *Mounter) PUT(path string, Func Handle) {
-	mounter.Routes = append(mounter.Routes, &Route{path, "put", Func})
+func (mounter *Mounter) PUT(path string, Funcs ...Handle) {
+	mounter.Routes = append(mounter.Routes, &Route{path, "put", Funcs})
 }
 
 //DELETE request handler
-func (mounter *Mounter) DELETE(path string, Func Handle) {
-	mounter.Routes = append(mounter.Routes, &Route{path, "delete", Func})
+func (mounter *Mounter) DELETE(path string, Funcs ...Handle) {
+	mounter.Routes = append(mounter.Routes, &Route{path, "delete", Funcs})
 }
